ea67cab0f6fe53d6d2d858d5f91f7592ddc1b1be
Oleksii Shevchenko <oleksiishevchenko93@gmail.com>
2023-03-01 23:05:52 +0200

    connect to DB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://shevchenkoaleksey9:sVhujV9PJgQ3Zv4u@cluster0.joyxmmb.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
