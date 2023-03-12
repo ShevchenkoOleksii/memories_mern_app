5ecb05c21f7685b3bf0d9c761dcf54048d84ddea
Oleksii Shevchenko <oleksiishevchenko93@gmail.com>
2023-03-12 22:46:56 +0200

    added creating post

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useSelector } from "react-redux";
+import { CircularProgress, Grid } from "@material-ui/core";
+
+import useStyles from "./styles";
+
+import Post from "./Post/Post";
+
+const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  return (
+    !posts.length ? <CircularProgress /> : (
+      <Grid
+        className={classes.container}
+        container
+        alignItems={'stretch'}
+        spacing={3}
+      >
+        {posts.map((post) => (
+          <Grid
+            key={post._id}
+            item
+            xs={12}
+            sm={6}
+          >
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
+};
+
+export default Posts;
